3c2e23f7a862ca10d7f1422d0eda9e46ac14770b
Ankit Choudhary अंकित चौधरी <60814508+ankitchoudhary-vcf@users.noreply.github.com>
2025-08-29 00:47:53 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,7 +1,9 @@
 import { exec } from "child_process";
 
 // BAD: command injection vulnerability
+// codeql [js/command-injection]
 function runCommand(userInput) {
+  // Potential command injection vulnerability
   exec(`ls ${userInput}`, (err, stdout) => {
     if (err) {
       console.error(err);
@@ -12,7 +14,9 @@ function runCommand(userInput) {
 }
 
 // BAD: SQL injection vulnerability
+// codeql [js/sql-injection]
 function unsafeQuery(userInput, db) {
+  // Potential SQL injection vulnerability
   db.query(
     "SELECT * FROM users WHERE name = '" + userInput + "'",
     (err, result) => {
@@ -26,7 +30,9 @@ function unsafeQuery(userInput, db) {
 }
 
 // BAD: unsafe eval usage
+// codeql [js/eval-detected]
 function runUserCode(userCode) {
+  // Potential unsafe eval usage
   eval(userCode);
 }
 
