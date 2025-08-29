555f54ecc930b67b5bf5c723c9644cef6109abce
Ankit Choudhary अंकित चौधरी <ac2000choudhary@gmail.com>
2025-08-29 14:35:57 +0530

    fix: remove/fix command injection - Removed vulnerable `runCommand` or replaced with safe implementation - Command injection alert (js/shell-command-constructed-from-input) should now be fixed - SQL injection (unsafeQuery) remains open

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { exec } from "child_process";
+// import { exec } from "child_process";
 // // ----------------------------
 // // BAD: command injection
 // // codeql [js/command-injection]
